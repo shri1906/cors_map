1d0de18cd94033246943655b83bd2f06c2a3eaf0
shri1906 <rajm46069@gmail.com>
2025-09-04 16:49:23 +0530

    map created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,12 @@
 import './App.css'
-import CorsMap from '../src/components/CorsMap.jsx'
+import CorsMap from './components/CorsMap.jsx'
 
 function App() {
 
   return (
      <div className="p-4">
-      <h1>CORS Live Tracker (India)</h1>
-      <CorsMap
-        mode="poll"
-        geojsonUrl="/cors.geojson"
-        pollSeconds={15}
-      />
+      <h1 className='text-center'>CORS Live Tracker (India)</h1>
+      <CorsMap />
     </div>
   )
 }
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: { exclude: ["@arcgis/core"] }, // prevent pre-bundling
-  build: { sourcemap: true }, // easier debugging
-  server: { port: 5173 },
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "node_modules/@arcgis/core/assets",
+          dest: "arcgis"  // will be copied into dist/arcgis
+        }
+      ]
+    })
+  ],
+  optimizeDeps: {
+    exclude: ["@arcgis/core"]
+  }
 });
